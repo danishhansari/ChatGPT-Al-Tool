3ca2b8e748276b04acec9980b24b89a00d57b6d7
Danish <dan71ish@gmail.com>
2023-05-13 20:55:17 +0530

    ..

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,9 +2,20 @@ import './App.css'
 
 function App() {
   return (
-    <div>
-      App
+    <>
+    <div className="bg-[#1A232E] h-screen py-6 relative sm:px-28 text-white overflow-hidden flex flex-col justify-between align-middle">
+    
+
+    
+    <div className='uppercase font-bold text-2xl text-center'>ChatGPT 2.0</div>
+
+    {/* Body */}
+    <div>Body</div>
+
+    {/* // input */}
+    <div>INput</div>
     </div>
+    </>
   )
 }
 
